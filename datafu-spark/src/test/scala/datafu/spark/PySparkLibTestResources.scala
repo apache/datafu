32257d8333a79390a0db9ee52d87bc1d1c94da05
Eyal Allweil <eyal@apache.org>
2022-10-19 20:44:34 +0300

    Update testing script and testing library versions

--- datafu-spark/src/test/scala/datafu/spark/PySparkLibTestResources.scala
+++ datafu-spark/src/test/scala/datafu/spark/PySparkLibTestResources.scala
@@ -29,10 +29,6 @@ object PathsResolver {
   val sparkSystemVersion = System.getProperty("datafu.spark.version")
   
   val py4js = Map(
-      "2.1.0" -> "0.10.4",
-      "2.1.1" -> "0.10.4",
-      "2.1.2" -> "0.10.4",
-      "2.1.3" -> "0.10.4",
       "2.2.0" -> "0.10.7",
       "2.2.1" -> "0.10.7",
       "2.2.2" -> "0.10.7",
@@ -45,9 +41,9 @@ object PathsResolver {
       "2.4.3" -> "0.10.8.1"
   )
 
-	val sparkVersion = if (sparkSystemVersion == null) "2.3.0" else sparkSystemVersion
+  val sparkVersion = if (sparkSystemVersion == null) "2.4.3" else sparkSystemVersion
   
-  val py4jVersion = py4js.getOrElse(sparkVersion, "0.10.6") // our default
+  val py4jVersion = py4js.getOrElse(sparkVersion, "0.10.8.1") // our default
   
   val pyspark = ResourceCloning.cloneResource(new File("data/pysparks/pyspark-" + sparkVersion + ".zip").toURI().toURL(),
     "pyspark_cloned.zip").getPath
